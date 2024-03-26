10e6a6afbcaa10410bdd4ad90253456def2d1a1b
Trupti <patiltruptib@gmail.com>
2024-03-25 23:55:35 -0700

    Channged logic of JWT functionality, Created JWT token at signIN page

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,30 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET_KEY } = process.env;
 
 /**
+ * Custom Middleware to verify JWT token
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+function verifyToken(req, res, next) {
+    //Get the 2nd element in the array after splitting the string "Bearer token"
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Token not provided" });
+    }
+    // Verify JWT token
+    jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            console.log("invalid jwt token.");
+            return res.status(403).json({ error: "Invalid JWT token.Token verification failed" });
+        }
+        // Attached the decoded user data from token to req.user
+        req.user = decoded;        
+        next();
+    });
+}
+
+/**
  * Adding new user to "users" table,
  * once user created successfully:Create JWT token and send in response
  * @param {A} req  
@@ -20,17 +44,7 @@ router.post("/register", async (req, res) => {
         const newUserId = result[0];
         //Fetch newly added user,if you get that means successful insertion
         const createdUser = await knex("users").where({ id: newUserId });
-        // Generate JWT token
-        const token = jwt.sign(
-            {               
-                email: createdUser.email,
-            },
-            JWT_SECRET_KEY,
-            {
-                expiresIn: "2h",
-            }
-        );      
-        res.status(201).json({ token });
+        res.status(201).json("New user created successfully!");
     } catch (error) {
         if (error.code === 'ER_DUP_ENTRY') {
             res.status(409).json({ error: 'User is already exists' });
@@ -44,14 +58,9 @@ router.post("/register", async (req, res) => {
  * From Login screen, verify if user in present in "users" table or not, 
  */
 router.get("/login", async (req, res) => {
-    try {   
-        //Get the 2nd element in the array after splitting the string "Bearer token"
-        const token = req.headers.authorization.split(" ")[1]; 
+    try {
         const email = req.query.userData.email;
         const password = req.query.userData.password;
-
-        console.log(token);
-
         const user = await knex("users").
             where({
                 email: email,
@@ -59,25 +68,45 @@ router.get("/login", async (req, res) => {
             });
 
         if (!user[0]) {
-            return res.status(401).json({ error: 'Invalid email or Password' });
-        }        
-        console.log(user[0]);
-        // Verify JWT token
-        jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
-            if (err) {
-                console.log("invalid jwt token.");
-               return res.status(401).json({ error: 'Invalid JWT token' });
+            return res.status(401).json({ error: 'User is not Autheticated.' });
+        }
+
+        // Generate JWT token, after Users successful authentication
+        const token = jwt.sign(
+            {
+                email: user[0].email,
+                id: user[0].id
+            },
+            JWT_SECRET_KEY,
+            {
+                expiresIn: "2h",
             }
-            if (decoded.id !== user.id) {
-               return res.status(409).json({ error: 'JWT token does not match user' });
-                console.log("user not authenticated.");
-            }            
-            res.status(201).json(user[0]);
-        } );
+        );
+        res.status(201).json({ token });
     } catch (error) {
-        res.status(500).send("Error in Signing in user." + error);
-        console.log(error);
+        res.status(500).send("Error in Signing in user." + error);      
     }
 });
 
+/**
+ * Added to get UserProfile data for given token
+ */
+router.get("/profile", verifyToken, async (req, res) => {
+    try {
+        const user = await knex("users").
+            where({
+                email: String(req.user.email),
+                id: Number(req.user.id)
+            });
+
+        if (!user[0]) {
+            return res.status(401).json({ error: 'User is not Authorized.' });
+        }
+        console.log("User Profile data", user[0]);
+        res.status(201).json(user[0]);
+
+    } catch (error) {
+        res.status(500).send("Error in Signing in user." + error);
+    }
+});
 
